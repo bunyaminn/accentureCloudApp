7d7e5efb469fa0140543c60261e8e8f19338e263
bunyamin ince <bunyamin.ince@accenture.com>
2024-03-22 17:36:48 +0000

    first commit

---
+++ DMCPodPlugin2/webapp/DMCPodPlugin2/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/dm/dme/podfoundation/component/production/ProductionUIComponent",
+	"sap/ui/Device"
+], function (ProductionUIComponent, Device) {
+	"use strict";
+
+	return ProductionUIComponent.extend("accenture.custom.podplugin2.DMCPodPlugin2.DMCPodPlugin2.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
---
+++ DMCPodPlugin2/webapp/DMCPodPlugin2/builder/PropertyEditor.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/model/resource/ResourceModel",
+    "sap/dm/dme/podfoundation/control/PropertyEditor"
+], function (ResourceModel, PropertyEditor) {
+    "use strict";
+    
+    var oFormContainer;
+
+    return PropertyEditor.extend( "accenture.custom.podplugin2.DMCPodPlugin2.DMCPodPlugin2.builder.PropertyEditor" ,{
+
+		constructor: function(sId, mSettings){
+			PropertyEditor.apply(this, arguments);
+			
+			this.setI18nKeyPrefix("customComponentListConfig.");
+			this.setResourceBundleName("accenture.custom.podplugin2.DMCPodPlugin2.DMCPodPlugin2.i18n.builder");
+			this.setPluginResourceBundleName("accenture.custom.podplugin2.DMCPodPlugin2.DMCPodPlugin2.i18n.i18n");
+		},
+		
+		addPropertyEditorContent: function(oPropertyFormContainer){
+			var oData = this.getPropertyData();
+			
+			this.addSwitch(oPropertyFormContainer, "backButtonVisible", oData);
+			this.addSwitch(oPropertyFormContainer, "closeButtonVisible", oData);
+						
+			this.addInputField(oPropertyFormContainer, "title", oData);
+			this.addInputField(oPropertyFormContainer, "text", oData);
+
+            oFormContainer = oPropertyFormContainer;
+		},
+		
+		getDefaultPropertyData: function(){
+			return {
+				
+				"backButtonVisible": true,
+				"closeButtonVisible": true,
+                "title": "DMCPodPlugin2",
+				"text": "DMCPodPlugin2"
+                
+			};
+		}
+
+	});
+});
